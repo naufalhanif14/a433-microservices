8e39e9fbc66284fec589630aac198b67a3631cd5
naufalhanif14 <naufalhanif1477.nh@gmail.com>
2022-11-29 22:21:05 +0800

    Microservice

--- build_push_image.sh
+++ build_push_image.sh
@@ -6,7 +6,7 @@ docker build -t item-app:v1 .
 # Melihat Daftar Image di Lokal
 docker images
 
-# Mengubah Nama Image Agar Sesuai Dengan Format Docker Hub
+# Mengubah Nama Image Agar Sesuai Dengan Format GitHub Packages
 docker tag item-app:v1 ghcr.io/naufalhanif14/item-app:v2
 
 # Mengekspor Github Token pada Variabel CR_PAT
@@ -15,5 +15,5 @@ export CR_PAT=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
 # Mencetak nilai dari Variabel CR_PAT Kemudian Outputnya Diteruskan ke Perintah Docker login Menggunakan Simbol Pipeline
 echo $CR_PAT | docker login ghcr.io -u naufalhanif14 --password-stdin
 
-# Mengunggah image ke Docker Hub
+# Mengunggah image ke GitHub Packages
 docker push ghcr.io/naufalhanif14/item-app:v2
