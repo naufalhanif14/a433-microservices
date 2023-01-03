22dc02959db1979b01b5a7443564bb95dbc4f0d0
naufalhanif14 <naufalhanif1477.nh@gmail.com>
2023-01-03 11:12:00 +0800

    Microservice

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Membuat Docker Image dari Dockerfile
+docker build -t karsajobs-ui:latest .
+
+# Mengubah Nama Image Agar Sesuai Dengan Format GitHub Packages
+docker tag karsajobs-ui:latest ghcr.io/naufalhanif14/karsajobs-ui:latest
+
+# Mengekspor Github Token pada Variabel CR_PAT
+export CR_PAT=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
+
+# Mencetak nilai dari Variabel CR_PAT Kemudian Outputnya Diteruskan ke Perintah Docker login Menggunakan Simbol Pipeline
+echo $CR_PAT | docker login ghcr.io -u naufalhanif14 --password-stdin
+
+# Mengunggah image ke GitHub Packages
+docker push ghcr.io/naufalhanif14/karsajobs-ui:latest
